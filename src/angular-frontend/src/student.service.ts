843e43c8e72e461612176d5afb83ac6858ec112b
ntloc <ntloc.developer@gmail.com>
2021-11-27 10:23:02 +0700

    update profile

--- src/angular-frontend/src/student.service.ts
+++ src/angular-frontend/src/student.service.ts
@@ -7,29 +7,29 @@ import { environment } from "./environments/environment";
 @Injectable({providedIn: 'root'})
 export class StudentService{
 
-  private apiStudentServer = 'http://localhost:8080/api/spring-boot' //connect to backend
+  private apiStudentServer = 'http://localhost:8080/api/spring-boot/student' //connect to backend
 
   constructor(private http: HttpClient){}
 
   public getAllStudent(): Observable<Student[]> //get All Student from backend
   {
-    return this.http.get<Student[]>(`${this.apiStudentServer}/student`); //localhost:8080/spring-boot/student
+    return this.http.get<Student[]>(`${this.apiStudentServer}`); //localhost:8080/spring-boot/student
   }
 
   public addStudent(student: Student): Observable<Student>
   {
-    return this.http.post<Student>(`${this.apiStudentServer}/student/add`,student);
+    return this.http.post<Student>(`${this.apiStudentServer}/add`,student);
   }
 
   public deleteStudent(studentId: number): Observable<void>
   {
-    return this.http.delete<void>(`${this.apiStudentServer}/student/delete/${studentId}`);
+    return this.http.delete<void>(`${this.apiStudentServer}/delete/${studentId}`);
   }
 
   public updateStudent(studentId: number, name: string, email: string,gender: string, dob: Date): Observable<Student>
   {
     const params= new HttpParams().set('name',name).set('email',email).set('gender',gender).set('dob',dob.toString());
-    return this.http.put<Student>(`${this.apiStudentServer}/student/update/${studentId}`,params);
+    return this.http.put<Student>(`${this.apiStudentServer}/update/${studentId}`,params);
   }
 
 
